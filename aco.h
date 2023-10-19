0c18575077c28d84efb95175205b925e7c6a7312
the-die <xiulieshan@gmail.com>
2023-10-19 15:27:10 +0800

    add notes to `aco.h`

--- aco.h
+++ aco.h
@@ -32,6 +32,7 @@
 extern "C" {
 #endif
 
+// https://semver.org/spec/v2.0.0.html
 #define ACO_VERSION_MAJOR 1
 #define ACO_VERSION_MINOR 2
 #define ACO_VERSION_PATCH 4
@@ -62,10 +63,14 @@ extern "C" {
     #error "platform no support yet"
 #endif
 
+// private stack
 typedef struct {
     void*  ptr;
     size_t sz;
     size_t valid_sz;
+
+    // The following variables are used for performance tracking.
+
     // max copy size in bytes
     size_t max_cpsz;
     // copy from share stack to this save stack
@@ -77,13 +82,33 @@ typedef struct {
 struct aco_s;
 typedef struct aco_s aco_t;
 
+/*
+ * guard_page_enabled: 1
+ * highptr: align to 16 bytes
+ *
+ * high
+ *  |            +-----------------+         ^    ^
+ *  |            |                 |         |    |
+ *  | highptr -> +-----------------+         |    |
+ *  |            |    protector    |         |    |
+ *  |  retptr -> +-----------------+         | sz |
+ *  |            |                 |         |    | real_sz
+ *  |            |                 |         |    |
+ *  |            |                 |         |    |
+ *  |            +-----------------+ <- ptr  v    |
+ *  |            |                 |              |
+ *  |            +-----------------+ <- realptr   v
+ *  v
+ * low
+ */
+
 typedef struct {
     void*  ptr;
     size_t sz;
-    void*  align_highptr;
-    void*  align_retptr; // return address
-    size_t align_validsz;
-    size_t align_limit; // ?
+    void*  align_highptr; // stack top
+    void*  align_retptr;  // return address
+    size_t align_validsz; // stack size
+    size_t align_limit;   // stack limit
     aco_t* owner;
 
     char guard_page_enabled;
@@ -115,10 +140,10 @@ struct aco_s{
     #error "platform no support yet"
 #endif
     aco_t* main_co;
-    void*  arg;
     char   is_end;
 
     aco_cofuncp_t fp;
+    void*  arg;
 
     aco_save_stack_t  save_stack;
     aco_share_stack_t* share_stack;
@@ -155,15 +180,19 @@ struct aco_s{
     #error "aco_attr_no_asan already defined"
 #endif
 #if defined(ACO_USE_ASAN)
+    // https://clang.llvm.org/docs/LanguageExtensions.html
+    // https://clang.llvm.org/docs/AddressSanitizer.html
+    // https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html
+    // https://gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html
     #if defined(__has_feature)
-        #if __has_feature(__address_sanitizer__)
+        #if __has_feature(address_sanitizer)
             #define aco_attr_no_asan \
-                __attribute__((__no_sanitize_address__))
+                __attribute__((no_sanitize_address))
         #endif
     #endif
     #if defined(__SANITIZE_ADDRESS__) && !defined(aco_attr_no_asan)
         #define aco_attr_no_asan \
-            __attribute__((__no_sanitize_address__))
+            __attribute__((no_sanitize_address))
     #endif
 #endif
 #ifndef aco_attr_no_asan
@@ -172,7 +201,19 @@ struct aco_s{
 
 extern void aco_runtime_test(void);
 
+// Initializes the libaco environment in the current thread.
+//
+// It will store the current control words of FPU and MXCSR into a thread-local global variable.
+
+// If the global macro ACO_CONFIG_SHARE_FPU_MXCSR_ENV is not defined, the saved control words would
+// be used as a reference value to set up the control words of the new co's FPU and MXCSR
+// (in aco_create) and each co would maintain its own copy of FPU and MXCSR control words during
+// later context switching.
+// If the global macro ACO_CONFIG_SHARE_FPU_MXCSR_ENV is defined, then all the co shares the same
+// control words of FPU and MXCSR. 
 extern void aco_thread_init(aco_cofuncp_t last_word_co_fp);
+
+// https://gcc.gnu.org/onlinedocs/gcc/Asm-Labels.html
 
 extern void* acosw(aco_t* from_co, aco_t* to_co) __asm__("acosw"); // asm
 
@@ -183,12 +224,25 @@ extern void aco_funcp_protector_asm(void) __asm__("aco_funcp_protector_asm"); //
 
 extern void aco_funcp_protector(void);
 
+// Equal to aco_share_stack_new2(sz, 1).
 extern aco_share_stack_t* aco_share_stack_new(size_t sz);
 
+// Creates a new share stack with a advisory memory size of sz in bytes and may have a guard page
+// (read-only) for the detection of stack overflow which is depending on the 2nd argument
+// guard_page_enabled.
 aco_share_stack_t* aco_share_stack_new2(size_t sz, char guard_page_enabled);
 
+// Destory the share stack sstk.
+//
+// Be sure that all the co whose share stack is sstk is already destroyed when you destroy the sstk.
 extern void aco_share_stack_destroy(aco_share_stack_t* sstk);
 
+// Create a new co.
+//
+// If it is a main_co you want to create, just call: aco_create(NULL, NULL, 0, NULL, NULL). Main co
+// is a special standalone coroutine whose share stack is the default thread stack. In the thread,
+// main co is the coroutine who should be created and started to execute before all the other
+// non-main coroutine does.
 extern aco_t* aco_create(
         aco_t* main_co,
         aco_share_stack_t* share_stack, 
@@ -199,6 +253,13 @@ extern aco_t* aco_create(
 // aco's Global Thread Local Storage variable `co`
 extern __thread aco_t* aco_gtls_co;
 
+// Yield from the caller main co and to start or continue the execution of co.
+// 
+// The caller of this function must be a main co and must be co->main_co. And the 1st argument co
+// must be a non-main co.
+//
+// The first time you resume a co, it starts running the function pointing by co->fp. If co has
+// already been yielded, aco_resume restarts it and continues the execution.
 aco_attr_no_asan
 extern void aco_resume(aco_t* resume_co);
 
@@ -209,16 +270,23 @@ extern void aco_resume(aco_t* resume_co);
     acosw((yield_co), (yield_co)->main_co);   \
 } while(0)
 
+// Yield the execution of co and resume co->main_co. The caller of this function must be a non-main
+// co. And co->main_co must not be NULL.
 #define aco_yield() do {        \
     aco_yield1(aco_gtls_co);    \
 } while(0)
 
+// Equal to (aco_get_co()->arg). And also, the caller of this function must be a non-main co.
 #define aco_get_arg() (aco_gtls_co->arg)
 
+// Return the pointer of the current non-main co. The caller of this function must be a non-main co.
 #define aco_get_co() ({(void)0; aco_gtls_co;})
 
+// Same as aco_get_co().
 #define aco_co() ({(void)0; aco_gtls_co;})
 
+// Destroy the co. The argument co must not be NULL. The private save stack would also been
+// destroyed if the co is a non-main co.
 extern void aco_destroy(aco_t* co);
 
 #define aco_is_main_co(co) ({((co)->main_co) == NULL;})
@@ -232,6 +300,8 @@ extern void aco_destroy(aco_t* co);
     aco_assert(0);                  \
 } while(0)
 
+// In addition do the same as aco_yield(), aco_exit() also set co->is_end to 1 thus to mark the co
+// at the status of "end".
 #define aco_exit() do {       \
     aco_exit1(aco_gtls_co); \
 } while(0)
